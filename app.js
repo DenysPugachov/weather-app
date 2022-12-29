eec69879da7bbb0c09ca2641a416900a32d3e3cc
Denys Pugachov <pugachovdenys@gmail.com>
2022-12-29 18:58:18 +0100

    #32 customise request data

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ const request = require("request")
 
 const url = "http://api.weatherstack.com/current?access_key=886ded7a7622bf45e9faeef35df7cec3&query=51.1605,16.9033"
 
-request({ url: url }, (error, response) => {
-    const data = JSON.parse(response.body)
-    console.log(data.current)
+request({ url: url, json: true }, (error, res) => {
+    const dataObj = res.body.current
+
+    console.log(`It is currently ${dataObj.temperature} degrees out. It is fillslike ${dataObj.feelslike} out there.`)
 })
