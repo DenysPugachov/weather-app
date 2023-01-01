7b8683fcfbb412822919f0e245cd75893aa247e2
Denys Pugachov <pugachovdenys@gmail.com>
2023-01-01 13:35:37 +0100

    #36 add geocode utils file

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const request = require("request")
+const geocode = require("./utils/geocode")
 
 const url = "http://api.weatherstack.com/current?access_key=886ded7a7622bf45e9faeef35df7cec3&query=51.1605,16.9033"
 const geocodeURL = "https://api.mapbox.com/geocoding/v5/mapbox.places/wroclaw.json?access_token=pk.eyJ1IjoiZGVuLTIxIiwiYSI6ImNrdjZvY2hsbjFlYWwybm8wNDZjMDhyamoifQ.vlC2ysnO6PU07Ln1ERWQrg&limit=1"
@@ -8,21 +9,29 @@ const geocodeURL = "https://api.mapbox.com/geocoding/v5/mapbox.places/wroclaw.js
 //         console.log(`Unable to connect to weather service.`)
 //     } else if (res.body.error) { // catch error from wether API
 //         console.log('Error :>> ', res.body.error.info);
-//     } else { // catch log level error (internet connectivity)
+//     } else { 
 //         const dataObj = res.body.current
 //         console.log(`It is currently ${dataObj.temperature} degrees out. \nIt is fillslike ${dataObj.feelslike} out there.`)
 //     }
 // })
 
-request({ url: geocodeURL, json: true }, (error, res) => {
-    if (error) {
-        console.log(`Unable to connect to the service.`)
-    } else if (res.body.features.length === 0) {
-        console.log(`Theare is not mathching result found. Please specify search term.`);
-    } else {
-        const latitude = res.body.features[0].center[0]
-        const longitude = res.body.features[0].center[1]
-        console.log('latitude :>> ', latitude);
-        console.log('longitude :>> ', longitude);
-    }
-}) 
+// request({ url: geocodeURL, json: true }, (error, res) => {
+//     if (error) {
+//         console.log(`Unable to connect to the service.`)
+//     } else if (res.body.features.length === 0) {
+//         console.log(`Theare is not mathching result found. Please specify search term.`);
+//     } else {
+//         const latitude = res.body.features[0].center[0]
+//         const longitude = res.body.features[0].center[1]
+//         console.log('latitude :>> ', latitude);
+//         console.log('longitude :>> ', longitude);
+//     }
+// }) 
+
+
+
+
+geocode("Wroclaw", (err, data) => {
+    console.log('data :>> ', data);
+    console.log('err :>> ', err);
+})
---
+++ utils/geocode.js
@@ -0,0 +1,23 @@
+const request = require("request")
+
+const geocode = (address, callback) => {
+    //safely parse string in to URI (? >> %3F)
+    const codedAddress = encodeURIComponent(address)
+    const url = `https://api.mapbox.com/geocoding/v5/mapbox.places/${codedAddress}.json?access_token=pk.eyJ1IjoiZGVuLTIxIiwiYSI6ImNrdjZvY2hsbjFlYWwybm8wNDZjMDhyamoifQ.vlC2ysnO6PU07Ln1ERWQrg&limit=1`
+
+    request({ url, json: true }, (err, res) => {
+        if (err) {
+            callback(`Unable to connect to the service. Please check your network.`, undefined)
+        } else if (res.body.features.length === 0) {
+            callback(`Theare is not mathching result found. Please specify search term.`, undefined)
+        } else {
+            callback(undefined, {
+                latitude: res.body.features[0].center[0],
+                longitude: res.body.features[0].center[1],
+                location: res.body.features[0].place_name,
+            })
+        }
+    })
+}
+
+module.exports = geocode
